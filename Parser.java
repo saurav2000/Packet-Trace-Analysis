759adb123b03ae68a5a48c70ca73a3b145a4132f
Saurav <musunurusaurav@gmail.com>
2019-10-11 01:10:40 +0530

    Added Parser

---
+++ Parser.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Parser
+{
+	public static ArrayList<ArrayList<String>> data = new ArrayList<>();
+
+	public static void read(String arg)
+	{
+		data.clear();
+		Scanner sc = null;
+		try
+		{
+			sc = new Scanner(new File(arg));
+		}catch(Exception e){}
+		sc.nextLine();
+		
+		while(sc.hasNextLine())
+		{
+			String line = sc.nextLine();
+			int k = 1;
+			ArrayList<String> columns = new ArrayList<>();
+			while(k<line.length())
+			{
+				int index = line.indexOf('"',k);
+				String temp = line.substring(k, index);
+				temp.trim();
+				columns.add(temp);
+				k = index+3;
+			}
+			data.add(columns);
+		}		
+	}
+}
