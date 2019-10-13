189b5c9779175341e130d50fd4b68bdbb7fef088
Saurav <musunurusaurav@gmail.com>
2019-10-13 19:27:15 +0530

    Added all files

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,7 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(s):
+def plot(s, sy, sx, l=False):
 	file1 = open(s, "r")
 	allLines = file1.readlines()
 
@@ -13,14 +12,45 @@ def plot(s):
 		xlist.append(float(xy[0]))
 		ylist.append(float(xy[1]))
 
-	plt.scatter(xlist, ylist)
-	plt.title('Scatter plot 1')
-	plt.ylabel('Prob(l<X)')
-	plt.xlabel('Packet length')
+	if(l):
+		plt.plot(xlist, ylist)
+	else:
+		plt.scatter(xlist, ylist)
+	plt.title(s)
+	plt.ylabel(sy)
+	plt.xlabel(sx)
 	plt.savefig(s+"_figure.png")
 	plt.clf()
 
-plot('file1.csv_q8_recv')
-plot('file2.csv_q8_recv')
-plot('file3.csv_q8_recv')
-# plt.savefig('q5_superimposed.png')
+plot('file1.csv_q4', 'P(Conn. Dur)<X', 'Connection Duration')
+plot('file2.csv_q4', 'P(Conn. Dur)<X', 'Connection Duration')
+plot('file3.csv_q4', 'P(Conn. Dur)<X', 'Connection Duration')
+
+plot('file1.csv_q5_send_vs_time', 'Incoming(Server) data size', 'Duration of conn.')
+plot('file2.csv_q5_send_vs_time', 'Incoming(Server) data size', 'Duration of conn.')
+plot('file3.csv_q5_send_vs_time', 'Incoming(Server) data size', 'Duration of conn.')
+plot('file1.csv_q5_send_vs_recv', 'Incoming(Server) data size', 'Outgoing(Server) data size')
+plot('file2.csv_q5_send_vs_recv', 'Incoming(Server) data size', 'Outgoing(Server) data size')
+plot('file3.csv_q5_send_vs_recv', 'Incoming(Server) data size', 'Outgoing(Server) data size')
+
+plot('file1.csv_q6', 'P(Inter-arr time)<X', 'Inter Arrival Time(Connection)')
+plot('file2.csv_q6', 'P(Inter-arr time)<X', 'Inter Arrival Time(Connection)')
+plot('file3.csv_q6', 'P(Inter-arr time)<X', 'Inter Arrival Time(Connection)')
+
+plot('file1.csv_q7', 'P(Inter-arr time)<X', 'Inter Arrival Time(Packets)')
+plot('file2.csv_q7', 'P(Inter-arr time)<X', 'Inter Arrival Time(Packets)')
+plot('file3.csv_q7', 'P(Inter-arr time)<X', 'Inter Arrival Time(Packets)')
+
+plot('file1.csv_q8_send', 'P(Packet length)<X', 'Incoming(Server) Packet length')
+plot('file2.csv_q8_send', 'P(Packet length)<X', 'Incoming(Server) Packet length')
+plot('file3.csv_q8_send', 'P(Packet length)<X', 'Incoming(Server) Packet length')
+plot('file1.csv_q8_recv', 'P(Packet length)<X', 'Outgoing(Server) Packet length')
+plot('file2.csv_q8_recv', 'P(Packet length)<X', 'Outgoing(Server) Packet length')
+plot('file3.csv_q8_recv', 'P(Packet length)<X', 'Outgoing(Server) Packet length')
+
+plot('file1.csv_q11_avg_queue_size', 'Avg Queue Size', 'Rho', True)
+plot('file2.csv_q11_avg_queue_size', 'Avg Queue Size', 'Rho', True)
+plot('file3.csv_q11_avg_queue_size', 'Avg Queue Size', 'Rho', True)
+plot('file1.csv_q11_avg_waiting_time', 'Avg Waiting Time', 'Rho', True)
+plot('file2.csv_q11_avg_waiting_time', 'Avg Waiting Time', 'Rho', True)
+plot('file3.csv_q11_avg_waiting_time', 'Avg Waiting Time', 'Rho', True)
